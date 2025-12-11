b1fc3a18ad9a1653ba227b1b027d45b986ccf735
Huu Nam <huunam@Huus-Mac-mini.local>
2025-12-11 21:44:03 +0700

    add logo

--- lib/screen/reminder/take_medicine_screen.dart
+++ lib/screen/reminder/take_medicine_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:medication_reminder/widget/gradient_text.dart';
 import '/config/global_color.dart';
 import '/config/global_text_style.dart';
 import '/model/reminder.dart';
@@ -27,109 +29,107 @@ class TakeMedicineScreen extends StatelessWidget {
             onTap: () {
               Get.back();
             },
-            child: SvgPicture.asset("assets/icons/back.svg",
-                color: Colors.black, width: 24.0, height: 24.0),
+            child: SvgPicture.asset(
+              "assets/icons/back.svg",
+              color: Colors.black,
+              width: 24.0,
+              height: 24.0,
+            ),
           ),
         ),
-        title: Text(
-          "Uống thuốc", // Localize this later
-          style: GlobalTextStyles.font18w600ColorBlack,
+        title: GradientText(
+          "Uống thuốc",
+          gradient: GlobalColors.linearPrimary2,
+          style: GlobalTextStyles.font20w600ColorWhite,
         ),
         centerTitle: true,
       ),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Icon or Image
-            Container(
-              padding: EdgeInsets.all(30.w),
-              decoration: BoxDecoration(
-                color: GlobalColors.bg1,
-                shape: BoxShape.circle,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Icon or Image
+              Lottie.asset(
+                'assets/lotties/dadengiouongthuoc.json',
+                fit: BoxFit.contain,
+                width: 360.w,
+                height: 360.w,
               ),
-              child: Icon(
-                Icons.medication,
-                size: 80.w,
-                color: GlobalColors.linearPrimary.colors.first,
-              ),
-            ),
-            40.verticalSpace,
-            
-            // Medicine Name
-            Text(
-              reminder.title ?? "Medicine",
-              style: GlobalTextStyles.font20w700ColorBlack.copyWith(
-                fontSize: 28.sp,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            16.verticalSpace,
-
-            // Time
-            Text(
-              "${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}",
-              style: GlobalTextStyles.font32w600ColorBlack.copyWith(
-                color: GlobalColors.linearPrimary.colors.first,
-                fontSize: 36.sp,
-              ),
-            ),
-            16.verticalSpace,
-
-            // Quantity
-            if (reminder.quantity != null)
+              40.verticalSpace,
+              // Medicine Name
               Text(
-                "Số lượng: ${reminder.quantity} viên", // Localize
-                style: GlobalTextStyles.font16w600ColorBlack.copyWith(
-                  color: GlobalColors.newtral,
-                  fontWeight: FontWeight.w400
+                reminder.title ?? "Medicine",
+                style: GlobalTextStyles.font20w700ColorBlack.copyWith(
+                  fontSize: 28.sp,
                 ),
+                textAlign: TextAlign.center,
               ),
-            
-            60.verticalSpace,
+              16.verticalSpace,
 
-            // Mark as Taken Button
-            GestureDetector(
-              onTap: () async {
-                await reminderCtl.markAsTaken(reminder);
-                Get.back();
-                Get.snackbar(
-                  "Thành công", 
-                  "Đã uống",
-                  backgroundColor: Colors.green.withOpacity(0.8),
-                  colorText: Colors.white,
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                height: 56.h,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  gradient: GlobalColors.linearPrimary2,
-                  borderRadius: BorderRadius.circular(28.r),
-                  boxShadow: [
-                    BoxShadow(
-                      color: GlobalColors.linearPrimary.colors.first.withOpacity(0.3),
-                      blurRadius: 10,
-                      offset: Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.check, color: Colors.white),
-                    8.horizontalSpace,
-                    Text(
-                      "Xác nhận đã uống", // Localize
-                      style: GlobalTextStyles.font16w600ColorWhite,
-                    ),
-                  ],
+              // Time
+              Text(
+                "${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}",
+                style: GlobalTextStyles.font32w600ColorBlack.copyWith(
+                  color: GlobalColors.linearPrimary.colors.first,
+                  fontSize: 36.sp,
                 ),
               ),
-            ),
-          ],
+              16.verticalSpace,
+
+              // Quantity
+              if (reminder.quantity != null)
+                Text(
+                  "Số lượng: ${reminder.quantity} viên", // Localize
+                  style: GlobalTextStyles.font16w600ColorBlack.copyWith(
+                      color: GlobalColors.newtral, fontWeight: FontWeight.w400),
+                ),
+
+              60.verticalSpace,
+
+              // Mark as Taken Button
+              GestureDetector(
+                onTap: () async {
+                  await reminderCtl.markAsTaken(reminder);
+                  Get.back();
+                  Get.snackbar(
+                    "Thành công",
+                    "Đã uống",
+                    backgroundColor: Colors.green.withOpacity(0.8),
+                    colorText: Colors.white,
+                  );
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 56.h,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    gradient: GlobalColors.linearPrimary2,
+                    borderRadius: BorderRadius.circular(28.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: GlobalColors.linearPrimary.colors.first
+                            .withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.check, color: Colors.white),
+                      8.horizontalSpace,
+                      Text(
+                        "Xác nhận đã uống", // Localize
+                        style: GlobalTextStyles.font16w600ColorWhite,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
